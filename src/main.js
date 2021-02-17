ee78ae3f525e197b404520e44e0b62919ef6d59f
ynl <1849475038@qq.com>
2021-02-17 12:20:18 +0800

    修复build项目时element-ui样式丢失的问题

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUi from 'element-ui'
 import App from './App'
 import router from './router'
-import './plugins/element.js'
 import axios from 'axios'
 import store from './store/index.js'
 Vue.config.productionTip = false
